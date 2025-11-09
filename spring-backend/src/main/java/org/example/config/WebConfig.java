e961e75039628cb0a0889b20142e70a8fae3cb7d
Kacper Przychodzki <kaetoupi@gmail.com>
2025-11-09 09:42:12 +0000

    Added jwt validation to backend endpoints

--- spring-backend/src/main/java/org/example/config/WebConfig.java
+++ spring-backend/src/main/java/org/example/config/WebConfig.java
@@ -3,12 +3,27 @@ package org.example.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Map;
+
+import static org.springframework.security.oauth2.core.authorization.OAuth2AuthorizationManagers.hasScope;
 
 
 @RequiredArgsConstructor
+@EnableWebSecurity
 @Configuration
 public class WebConfig {
 
@@ -18,8 +33,31 @@ public class WebConfig {
 //        TODO: actuator
 //        TODO: Default - require creds
         return httpSecurity
-                .authorizeHttpRequests((auth) -> auth.anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auth) -> auth
+                                .requestMatchers("/actuator/**").anonymous()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                        Customizer.withDefaults()
+//                        jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+                ))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri("http://localhost:8020/realms/spreact/protocol/openid-connect/certs").build();
     }
 }
