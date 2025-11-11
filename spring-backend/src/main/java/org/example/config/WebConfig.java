fd8bf87479b085249b0aee703d2ab3eaeacc6e17
Kacper Przychodzki <kaetoupi@gmail.com>
2025-11-11 12:03:45 +0000

    Updated tests to account for authz Added missing test to ensure that actuator endpoints are not protected

--- spring-backend/src/main/java/org/example/config/WebConfig.java
+++ spring-backend/src/main/java/org/example/config/WebConfig.java
@@ -3,23 +3,16 @@ package org.example.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.Collection;
-import java.util.Map;
-
-import static org.springframework.security.oauth2.core.authorization.OAuth2AuthorizationManagers.hasScope;
 
 
 @RequiredArgsConstructor
@@ -54,6 +47,7 @@ public class WebConfig {
         return jwtAuthenticationConverter;
     }
 
+    @Profile("!test")
     @Bean
     public JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withJwkSetUri("http://localhost:8020/realms/spreact/protocol/openid-connect/certs").build();
