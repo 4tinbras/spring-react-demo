f51c29467f8c3d059272bd63c8c7a36249180080
Kacper Przychodzki <kaetoupi@gmail.com>
2025-11-14 21:23:48 +0000

    Refactor main page to hold different content based on interactions

--- react-front/app/page.tsx
+++ react-front/app/page.tsx
@@ -1,10 +1,28 @@
+'use client'
+
 import ContactBlock from "@/app/ContactsBlock";
 import NavBar from "@/app/NavBar";
+import {useState} from "react";
+import LoginBlock from "@/app/LoginBlock";
 // import 'bootstrap/dist/css/bootstrap.min.css'; //not necessary if added CDN link
 
 export default function Home() {
 
-  return (
+    const [activeTab, setActiveTab] = useState("HOME");
+    const [accessToken, setAccessToken] = useState("");
+
+    const TABS = {
+        'HOME': <ContactBlock accessToken={accessToken}/>,
+        'LOGIN': <LoginBlock setAccessToken={setAccessToken}/>
+    }
+
+    {/*https://react.dev/learn/synchronizing-with-effects
+    TODO: add interval checkup on access token expiry and show pop-up or try to refresh
+    */
+    }
+
+
+    return (
       <>
           <link
               rel="stylesheet"
@@ -13,9 +31,12 @@ export default function Home() {
               crossOrigin="anonymous"
           />
           <div>
-              <NavBar></NavBar>
+              <NavBar activeTab={activeTab} onNavbarClick={setActiveTab}></NavBar>
               <div className={'container justify-content-center d-flex my-5'}>
-                  <ContactBlock></ContactBlock>
+                  {
+                      // @ts-ignore
+                      TABS[activeTab]
+                  }
               </div>
           </div>
       </>
