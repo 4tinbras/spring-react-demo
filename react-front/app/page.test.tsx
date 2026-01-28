de9f14a4f42487201b33cc053639b9d249a7b1f0
Kacper Przychodzki <kaetoupi@gmail.com>
2026-01-28 12:43:41 +0000

    Updated main page tests

--- react-front/app/page.test.tsx
+++ react-front/app/page.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {fireEvent, render, screen} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import Home from "@/app/page";
 
 describe('Home component ', () => {
@@ -9,31 +9,7 @@ describe('Home component ', () => {
             <Home></Home>
         );
 
-        await screen.findByText('Home');
-        expect(screen.getByText('Home')).toBeInTheDocument();
-        expect(screen.getByText('Login')).toBeInTheDocument();
-
-        expect(screen.getByRole('paragraph')).toHaveTextContent('Please authenticate yourself in login tab.')
-        // expect(screen.getByRole('paragraph')).toHaveTextContent('No contacts found so far.')
-    })
-
-    it('correctly changes content based on navigation interactions', async () => {
-
-        const component = render(
-            <Home></Home>
-        );
-
-        await screen.findByText('Home');
-        expect(screen.getByText('Home')).toBeInTheDocument();
-        expect(screen.getByText('Login')).toBeInTheDocument();
-
-        // ACT
-        const loginLink = screen.getByText('Login')
-        fireEvent.click(loginLink);
-
-        expect(screen.queryByRole('paragraph', {name: 'Please authenticate yourself in login tab.'})).not.toBeInTheDocument();
-        // expect(screen.queryByRole('paragraph', {name: 'No contacts found so far.'})).not.toBeInTheDocument();
-
+        await screen.findByText('Hello world!');
     })
 
 });
