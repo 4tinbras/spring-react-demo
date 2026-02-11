297b46892edcf9b818a54a6d359dddf2ca8244e7
Kacper Przychodzki <kaetoupi@gmail.com>
2026-02-11 18:05:12 +0000

    Changed the way interchangeable services are annotated

--- spring-backend/src/main/java/org/example/messaging/NoopMessagingService.java
+++ spring-backend/src/main/java/org/example/messaging/NoopMessagingService.java
@@ -1,12 +1,8 @@
 package org.example.messaging;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Profile("!kafka-msg")
 @Slf4j
-@Service
 public class NoopMessagingService implements MessagingService {
 
     @Override
