52523887bc7661ecb84716bcbf56d26280d4668a
Kacper Przychodzki <kaetoupi@gmail.com>
2026-08-25 19:12:13 +0100

    Added styling to prevent odd spacing in navbar

--- react-front/app/NavBar.tsx
+++ react-front/app/NavBar.tsx
@@ -3,6 +3,7 @@
 import React, {useState} from 'react';
 import {Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem} from 'reactstrap';
 import Link from "next/link";
+import "./src/styles/navigation-header.css";
 import {useAuthZ} from "@/app/StateManagement";
 
 export default function NavBar() {
@@ -15,27 +16,31 @@ export default function NavBar() {
 
     return (
         <Navbar color="dark" dark expand="md">
-            <NavbarBrand href="/" className="me-auto">
-                spreact
-            </NavbarBrand>
-            <NavbarToggler onClick={() => {
-                setIsOpen(!isOpen)
-            }}/>
-            <Collapse isOpen={isOpen} navbar>
-                <Nav className="justify-content-start" style={{width: "100%"}} navbar pills>
-                    <NavItem>
-                        {activeTab === 'HOME' && (
-                                <Link href="/contacts" className={'active'}
-                                      onClick={() => setActiveTab('HOME')}>Home</Link>)
-                            || (<Link href="/contacts">Home</Link>)}
-                    </NavItem>
-                    <NavItem>
-                        {activeTab === 'LOGIN' && (
-                                <Link href="/login" className={'active'} onClick={() => setActiveTab('LOGIN')}>Login</Link>)
-                            || (<Link href="/login">Login</Link>)}
-                    </NavItem>
-                </Nav>
-            </Collapse>
+            <div className="top-container display-flex">
+                <NavbarBrand href="/">
+                    spreact
+                </NavbarBrand>
+                <NavbarToggler onClick={() => {
+                    setIsOpen(!isOpen)
+                }}/>
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="justify-content-start" style={{width: "100%"}} navbar pills>
+                        <NavItem>
+                            {/*TODO logic deciding whether class is needed requires new solution*/}
+                            {activeTab === 'HOME' && (
+                                    <Link href="/contacts" className={'active'}
+                                          onClick={() => setActiveTab('HOME')}>Home</Link>)
+                                || (<Link href="/contacts">Home</Link>)}
+                        </NavItem>
+                        <NavItem>
+                            {activeTab === 'LOGIN' && (
+                                    <Link href="/login" className={'active'}
+                                          onClick={() => setActiveTab('LOGIN')}>Login</Link>)
+                                || (<Link href="/login">Login</Link>)}
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </div>
         </Navbar>
     )
 }
