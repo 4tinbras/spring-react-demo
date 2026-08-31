b58de0e7e08f518b9a92ccfa66d6c4f65b58104e
Kacper Przychodzki <kaetoupi@gmail.com>
2026-08-31 17:58:30 +0100

    Added naive implementation of account controller

---
+++ spring-backend/src/main/java/org/example/account/AccountService.java
@@ -0,0 +1,32 @@
+package org.example.account;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.persistence.Account;
+import org.example.persistence.AccountRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    public List<Account> findAll() {
+        return accountRepository.findAll();
+    }
+
+    public Account save(final Account toBeSaved) {
+        return accountRepository.save(toBeSaved);
+    }
+
+    public void deleteById(String id) {
+        accountRepository.deleteById(id);
+    }
+
+    public Optional<Account> findByUuid(String uuid) {
+        return accountRepository.findById(uuid);
+    }
+}
