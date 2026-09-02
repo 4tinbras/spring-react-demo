50364842e41a5f1a5476fb229acf3171e40e7995
Kacper Przychodzki <kaetoupi@gmail.com>
2026-09-02 16:27:16 +0100

    Add test to check that openapi definition is available

--- spring-backend/src/test/java/org/example/ContextTest.java
+++ spring-backend/src/test/java/org/example/ContextTest.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,12 +25,17 @@ public class ContextTest {
     @Autowired
     private MockMvc mockMvc;
 
-    //TODO: Stopped working with the spring update
-    @Disabled
     @Test
-    void getHealthWithoutAuthZ_Returns200() throws Exception {
+    void getHealthWithoutAuthZ_Returns200() {
         ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
                 "http://localhost:" + this.mgt + "/actuator/health", Map.class);
+        Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
+    }
+
+    @Test
+    void getOpenApiDescription_Returns200() {
+        ResponseEntity<Map> entity = this.testRestTemplate.getForEntity(
+                "http://localhost:8080/v3/api-docs", Map.class);
         Assertions.assertEquals(HttpStatus.OK, entity.getStatusCode());
     }
 }
