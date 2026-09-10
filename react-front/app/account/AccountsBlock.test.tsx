d03c312fa36bd5875859ba3693de370bf0faada7
Kacper Przychodzki <kaetoupi@gmail.com>
2026-09-10 12:48:30 +0100

    Included account inspection in main page view

--- react-front/app/account/AccountsBlock.test.tsx
+++ react-front/app/account/AccountsBlock.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from "@testing-library/react";
+import {act, fireEvent, render, screen} from "@testing-library/react";
 import {
     AccountsDispatchContext,
     AccountsProvider,
@@ -8,8 +8,49 @@ import {
 } from "@/app/StateManagement";
 import React from "react";
 import AccountsBlock from "@/app/account/AccountsBlock";
-import {AccountBlockActions} from "@/app/utils";
-import {JWTPayload, jwtVerify, JWTVerifyResult} from "jose";
+import {Account, AccountBlockActions, ErrorResp} from "@/app/utils";
+import {JWTPayload, JWTVerifyResult} from "jose";
+import {setupServer} from "msw/node";
+import {http, HttpResponse} from "msw";
+
+const server = setupServer();
+
+const getAllAccounts = http.get<{}, never, Account[] | ErrorResp>('http://localhost:8080/accounts', function* (request) {
+    let response: Account | ErrorResp;
+    console.log('entered response mocking')
+
+    if (request.request.headers.get('Authorization') === 'Bearer accessToken') {
+        let response1 = {contactDetails: [1, 2], ownersFirstName: "Tom", ownersSurname: "Smith", uuid: "1"}
+        let response2 = {contactDetails: [2, 3], ownersFirstName: "Joe", ownersSurname: "Doe", uuid: "2"}
+        return HttpResponse.json([response1, response2])
+    } else {
+        console.log('setting response to invalid')
+        return HttpResponse.json({error: 'Not authorised'}, {status: 401})
+    }
+},)
+
+const getSingleAccount = http.get<{}, never, Account | ErrorResp>('http://localhost:8080/account/1', function* (request) {
+    let response: Account | ErrorResp;
+    console.log('entered response mocking')
+
+    if (request.request.headers.get('Authorization') === 'Bearer accessToken') {
+        let response = {contactDetails: [1, 2], ownersFirstName: "Tom", ownersSurname: "Smith", uuid: "1"}
+        return HttpResponse.json(response)
+    } else {
+        console.log('setting response to invalid')
+        return HttpResponse.json({error: 'Not authorised'}, {status: 401})
+    }
+},)
+
+server.use(getSingleAccount);
+server.use(getAllAccounts);
+
+
+beforeAll(() => server.listen())
+// TODO: that deregisters handlers declared with use(), so conflicts with current setup
+// reset any request handlers that are declared as a part of the tests
+// afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 const correctValidatedJWTPayload: JWTVerifyResult<JWTPayload> = {
     payload: {
@@ -22,10 +63,6 @@ const correctValidatedJWTPayload: JWTVerifyResult<JWTPayload> = {
 jest.mock('jose', () => {
     return {
         createRemoteJWKSet: jest.fn(),
-        // jwtVerify: jest.fn(),
-        // jwtVerify: jest.fn(() =>  correctValidatedJWTPayload ),
-        // jwtVerify: jest.fn((one, two, three) =>  correctValidatedJWTPayload ),
-        // jwtVerify: jest.fn(() => new Promise<JWTVerifyResult<JWTPayload>>(() => correctValidatedJWTPayload)),
         jwtVerify: jest.fn().mockResolvedValue(
             {
                 payload: {
@@ -35,12 +72,6 @@ jest.mock('jose', () => {
                 protectedHeader: {alg: "RS256"}
             }
         )
-        // jwtVerify: jest.fn(() =>
-        //     new Promise<JWTVerifyResult<JWTPayload>>((one, two, three) => correctValidatedJWTPayload )),
-        // then: jest.fn(result => {
-        //     console.log("jwt payload:" + result);
-        //     return result.payload;
-        // }),
     }
 });
 
@@ -93,8 +124,6 @@ const adminTokenPresentProps: AuthZContextProps = {
 describe('AccountsBlock ', () => {
     it('for an end-user view shows his own account retrieval button', async () => {
 
-        console.log(jwtVerify("1", {}, {}))
-
         customRender(<AccountsDispatchContext.Consumer>
                 {value => <AuthZContext.Consumer>
                     {value => <AccountsBlock/>}
@@ -105,10 +134,22 @@ describe('AccountsBlock ', () => {
 
         expect(screen.getByText('Get your Account'));
         expect(screen.queryByText('Get All Accounts')).not.toBeInTheDocument();
+
+        const contactsButton = screen.getByRole('button', {name: 'Get your Account'})
+
+        act(() => {
+            fireEvent.click(contactsButton)
+        });
+
+        await screen.findByText('Tom');
+        expect(screen.queryByText('Smith'));
+        expect(screen.queryByText('No Contact Details found'));
+        expect(screen.queryByText('Personal details'));
+        expect(screen.queryByText('Account configuration')).not.toBeInTheDocument();
+
     })
 
     it('for an administrative view shows all accounts retrieval button', async () => {
-        console.log(jwtVerify("1", {}, {}))
 
         customRender(<AccountsDispatchContext.Consumer>
                 {value => <AuthZContext.Consumer>
@@ -119,10 +160,21 @@ describe('AccountsBlock ', () => {
 
         expect(screen.getByText('Get your Account'));
         expect(screen.getByText('Get All Accounts'));
+
+        const contactsButton = screen.getByRole('button', {name: 'Get your Account'})
+
+        act(() => {
+            fireEvent.click(contactsButton)
+        });
+
+        await screen.findByText('Tom');
+        expect(screen.queryByText('Smith'));
+        expect(screen.queryByText('No Contact Details found'));
+        expect(screen.queryByText('Personal details'));
+        expect(screen.queryByText('Account configuration')).toBeInTheDocument();
     })
 
     it('for missing token shows info header', async () => {
-        console.log(jwtVerify("1", {}, {}))
 
         customRender(<AccountsDispatchContext.Consumer>
                 {value => <AuthZContext.Consumer>
--- react-front/app/account/AccountsBlock.tsx
+++ react-front/app/account/AccountsBlock.tsx
@@ -2,12 +2,14 @@
 
 import {useAccounts, useAuthZ} from "@/app/StateManagement";
 import {AccountBlockActions, FormStatus} from "@/app/utils";
+import AccountInspection from "@/app/account/AccountInspection";
+import AccountsList from "@/app/account/AccountsList";
 
 
 export default function AccountsBlock({}: {}) {
 
     const getAccountsEndpoint = `${process.env.NEXT_PUBLIC_BACKEND_HOST}` + "/accounts";
-    const getAccountEndpoint = `${process.env.NEXT_PUBLIC_BACKEND_HOST}` + "/account";
+    const getAccountEndpoint = `${process.env.NEXT_PUBLIC_BACKEND_HOST}` + "/account/1";
     const {authZToken, tokenPayload} = useAuthZ();
     const accessToken = authZToken;
     // const decryptedAccessToken: JWTPayload = getValidatedJWT(accessToken);
@@ -51,10 +53,25 @@ export default function AccountsBlock({}: {}) {
                     </button>
                     <button onClick={() => handleGetAccounts(true)} className={'button-primary'}>Get All Accounts
                     </button>
+
+                    {/*    hidden inspection element*/}
+                    {state.inspectedAccount !== undefined && (
+                        <AccountInspection inspectedAccount={state.inspectedAccount}
+                                           inspectorIsAdmin={true}></AccountInspection>
+                    )}
+                    {/*    hidden list element*/}
+                    {state.accounts !== undefined && (
+                        <AccountsList accounts={state.accounts}></AccountsList>
+                    )}
                 </>)
                 || (<>
                     {/*    retrieve only relevant account*/}
                     <button onClick={() => handleGetAccounts(false)} className={'button-primary'}>Get your Account
+
+                        {state.inspectedAccount !== undefined && (
+                            <AccountInspection inspectedAccount={state.inspectedAccount}
+                                               inspectorIsAdmin={false}></AccountInspection>
+                        )}
                     </button>
                 </>)) || (
                 <><h1>You need to login in first</h1></>)
